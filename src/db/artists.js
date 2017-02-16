6eaa6ba52d1d6cdd4033fd269950b41cd21b9225
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2017-02-16 11:00:58 -0500

    chore

--- src/db/artists.js
+++ src/db/artists.js
@@ -7,15 +7,15 @@ export default [
 		featured: true
 	},
 	{
-		name: 'DJ SEINFELD',
-		locale: 'SE',
+		name: 'DJ BORING',
+		locale: 'UK',
 		offering: 'DJ',
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: true
 	},
 	{
-		name: 'DJ BORING',
-		locale: 'UK',
+		name: 'DJ SEINFELD',
+		locale: 'SE',
 		offering: 'DJ',
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: true
@@ -26,6 +26,48 @@ export default [
 		offering: 'DJ',
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: true
+	},
+	{
+		name: 'DJ ASTRALLL',
+		locale: 'AUS',
+		offering: 'DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
+	},
+	{
+		name: 'GREY PEOPLE',
+		locale: 'US',
+		offering: 'DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
+	},
+	{
+		name: 'BALTRA',
+		locale: 'US',
+		offering: 'LIVE + DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
+	},
+	{
+		name: 'DJ BORING',
+		locale: 'UK',
+		offering: 'DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
+	},
+	{
+		name: 'DJ SEINFELD',
+		locale: 'SE',
+		offering: 'DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
+	},
+	{
+		name: 'DJ SWAGGER',
+		locale: 'DE',
+		offering: 'DJ',
+		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
+		featured: false
 	},
 	{
 		name: 'DJ ASTRALLL',
