238d920ffd75803d7c98c39d04e3ee587e05adae
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2017-02-28 17:42:44 -0500

    added image

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,3 +1,4 @@
 export default [
-	'http://res.cloudinary.com/luxip6j0n/image/upload/v1486578771/Untitled-4_vauflx.png'
+	'http://res.cloudinary.com/luxip6j0n/image/upload/v1486578771/Untitled-4_vauflx.png',
+  	'http://i63.tinypic.com/2dl6u7b.jpg'
 ]
