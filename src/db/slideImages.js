f8b0aabb8f64c6e49f44c812d43eb4837c739d32
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2017-03-24 11:46:36 -0400

    added obama to slider

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,3 +1,4 @@
 export default [
-	'http://res.cloudinary.com/luxip6j0n/image/upload/v1489280722/season2_zduhir.png'
+	'http://res.cloudinary.com/luxip6j0n/image/upload/v1489280722/season2_zduhir.png',
+  	'https://media1.s-nbcnews.com/j/newscms/2014_18/418791/140504-obama-correspondents-dinner-750a_53ba5a0e11b559d8a78cdb2962b30bfd.nbcnews-ux-2880-1000.jpg'
 ]
