60d899e35bb8e78add826a214aeda24fc43d1465
PalisadeAgency <r@palisadeagency.com>
2017-03-25 11:08:59 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,4 +1,4 @@
 export default [
 	'http://res.cloudinary.com/luxip6j0n/image/upload/v1489280722/season2_zduhir.png',
-  	'https://media1.s-nbcnews.com/j/newscms/2014_18/418791/140504-obama-correspondents-dinner-750a_53ba5a0e11b559d8a78cdb2962b30bfd.nbcnews-ux-2880-1000.jpg'
+  	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454488/Palisade_Runner_w0s9m5.png'
 ]
