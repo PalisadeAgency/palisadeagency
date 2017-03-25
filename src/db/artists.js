9578f7442f4534496cfb231562c71bffdb567fa8
PalisadeAgency <r@palisadeagency.com>
2017-03-25 14:51:07 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -307,7 +307,7 @@ export default [
 			twitter: 'null',
 			soundcloud: 'null'
 		},
-		bioOne: 'A turn of the century house lable based out of the UK. Lost Palms and Mall Grab's Steel City Dance Disks are sublables.',
+		bioOne: 'A turn of the century house lable based out of the UK. Lost Palms and Mall Grab s Steel City Dance Disks are sublabels.',
 		bioTwo: '',
 		agent: 'RYAN OTIS, MAT CONSTANCE',
 		col: 3,
