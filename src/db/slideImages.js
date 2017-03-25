58bbef0b27315224d296125ad338991ebacd1254
PalisadeAgency <r@palisadeagency.com>
2017-03-25 11:17:32 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,4 +1,5 @@
 export default [
 	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454752/fly_1_azypyj.png',
   	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454488/Palisade_Runner_w0s9m5.png'
+  	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490455024/boring_web_hgyf5z.png'
 ]
