72413f23479d49b4003979843177ecd8454579c8
PalisadeAgency <r@palisadeagency.com>
2017-03-25 11:12:51 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,4 +1,4 @@
 export default [
-	'http://res.cloudinary.com/luxip6j0n/image/upload/v1489280722/season2_zduhir.png',
+	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454752/fly_1_azypyj.png',
   	'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454488/Palisade_Runner_w0s9m5.png'
 ]
