a50b785b4a6bf76b722f95efdbdd95e0d504feba
PalisadeAgency <r@palisadeagency.com>
2017-03-26 00:30:26 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -383,7 +383,7 @@ export default [
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: false,
 		images: [
-			"https://res.cloudinary.com/luxip6j0n/image/upload/v1487314771/deejay-astral_dnyjsa.jpg"
+			"http://res.cloudinary.com/drh25z8fo/image/upload/v1490502599/x-coast_dlfpsz.png"
 		],
 		social: {
 			instagram: 'null',
