e65dbf26fb375d5a29f66c54bc9928796d6d511a
PalisadeAgency <r@palisadeagency.com>
2017-04-06 12:05:02 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -244,7 +244,7 @@ export default [
 			twitter: 'www.twitter.com/ballerasstapes',
 			soundcloud: 'www.soundcloud.com/pepmanpepe'
 		},
-		bioOne: 'Based out of Brighton, Pépe references several influences on which he draws upon for his work, including: 80’s Japanese music, jazz, and ambient as genres; and YMO, Kyle Hall, and Kerri Chandler as musicians. He’s put out work on Sprung Records, Renaissance, as well as In Records Paris, leaving his faithful following hungry for more. A recent standout of Pépe’s is a gorgeous and colourful house track entitled “Linda’s Retreat” that’s awaiting official release.',
+		bioOne: 'Based out of Brighton, Pépe references several influences including: 80’s Japanese music, jazz, and ambient as genres; and YMO, Kyle Hall, and Kerri Chandler as musicians. He’s put out work on Sprung Records, Renaissance, as well as In Records Paris, leaving his faithful following hungry for more. A recent standout of Pépe’s is a gorgeous and colourful house track entitled “Linda’s Retreat” that’s awaiting official release.',
 		bioTwo: '',
 		agent: 'RYAN OTIS',
 		col: 4,
