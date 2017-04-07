b8d120a9f84581e23ef72c78cfcfad0dc0887163
PalisadeAgency <r@palisadeagency.com>
2017-04-07 01:09:32 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,5 +1,5 @@
 export default [
 	'http://res.cloudinary.com/drh25z8fo/image/upload/v1491541653/Palisade_Web_Banner-Recovered_eulvt2.png'
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1490454752/fly_1_azypyj.png',
-  	'http://res.cloudinary.com/drh25z8fo/image/upload/v1491540996/palisade_web23_kmjgia.png'
+  	'http://res.cloudinary.com/drh25z8fo/image/upload/v1491541658/asdlfkjffdsfdd_dq4ffy.png'
 ]
