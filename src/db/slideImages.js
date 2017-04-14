3472c211aeb084b0419bc1dbbce06daf7aa1118f
PalisadeAgency <r@palisadeagency.com>
2017-04-14 15:27:33 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,4 +1,5 @@
 export default [
 	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492185795/Artboard_1_300x_vzwc3j.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492186245/asdfasdfggg_ivw5o5.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1492198000/web_front_ggw4u9.png',
 ]
