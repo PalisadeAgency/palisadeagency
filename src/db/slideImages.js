45d2d1b870d03fa4a948600cdfcd24f5527a50de
PalisadeAgency <r@palisadeagency.com>
2017-04-14 11:51:38 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,3 +1,4 @@
 export default [
 	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492184873/season_1_lopof6.png',
+ 'http://res.cloudinary.com/drh25z8fo/image/upload/v1492185041/Palisade_front_web_page_xvsrxo.png',
 ]
