c52879f755d41c22e21d548f33c24ba0d641e2e4
PalisadeAgency <r@palisadeagency.com>
2017-04-15 16:11:41 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -384,7 +384,7 @@ export default [
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: false,
 		images: [
-		"http://res.cloudinary.com/drh25z8fo/image/upload/v1492202276/asdfasdfagggggg_xparzk.png"
+		"http://res.cloudinary.com/drh25z8fo/image/upload/v1492287085/Website_artist_page_hn6xbp.png"
 		],
 		social: {
 			instagram: 'www.instagram.com/roy_fokker',
