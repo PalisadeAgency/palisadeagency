8886844b91a6fbae40d31baf5641c617116e0ad2
PalisadeAgency <r@palisadeagency.com>
2017-04-15 01:54:32 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -342,7 +342,7 @@ export default [
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
 		featured: false,
 		images: [
-			"http://res.cloudinary.com/drh25z8fo/image/upload/v1490504957/sonderr_ijax8o.png"
+			"http://res.cloudinary.com/drh25z8fo/image/upload/v1492235649/sonderrr_hgmwis.png"
 		],
 		social: {
 			instagram: 'www.instagram.com/sonderraus/',
