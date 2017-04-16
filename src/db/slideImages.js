22b80b5a9dfaa4010449753a140925658031b614
PalisadeAgency <r@palisadeagency.com>
2017-04-16 16:07:37 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,3 +1,4 @@
 export default [
 	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373107/dj_boring_front_frriz3.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
 ]
