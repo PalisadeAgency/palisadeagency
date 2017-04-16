b660375b395831fcad80fde600001859e2512022
PalisadeAgency <r@palisadeagency.com>
2017-04-16 12:35:58 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,5 +1,6 @@
 export default [
-	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492237186/boring_fronttsss_veyavj.png',
+	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492185795/Artboard_1_300x_vzwc3j.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1492237186/boring_fronttsss_veyavj.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492186245/asdfasdfggg_ivw5o5.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492198000/web_front_ggw4u9.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492198421/pasldifiasdf_ux60fi.png',
