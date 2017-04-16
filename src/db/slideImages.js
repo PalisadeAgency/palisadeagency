d7a7d3fc4bfcab54f6f84f7e160ca3e5a7c67dd2
PalisadeAgency <r@palisadeagency.com>
2017-04-16 16:12:28 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,5 +1,6 @@
 export default [
-	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373107/dj_boring_front_frriz3.png',
+	'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373518/palisade_nkry0x.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373107/dj_boring_front_frriz3.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373308/DJ_Boring_frontttsad_wqusim.png',
 ]
