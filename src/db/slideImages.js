f74abbdf1e0e6eb6586137cce2d89efa8c3456dd
PalisadeAgency <r@palisadeagency.com>
2017-04-18 13:29:41 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,5 +1,6 @@
 export default [
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373518/palisade_nkry0x.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1492536561/armadndndndndd_l3m3pb.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492408146/Shall_not_faderrr_cglukg.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373107/dj_boring_front_frriz3.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
