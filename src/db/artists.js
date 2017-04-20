41da509f8b3133919cb4da93aedc11e81e8e0e7f
PalisadeAgency <r@palisadeagency.com>
2017-04-19 22:31:22 -0400

    Updated artists.js

--- src/db/artists.js
+++ src/db/artists.js
@@ -210,7 +210,7 @@ export default [
 		region: "North America"
 	},
   {
-		name: 'Natural Sciences 🐬',
+		name: 'Natural Sciences',
 		locale: '(UK)',
 		offering: 'DJ',
 		description: 'Mixmag names "Fade Away" the 94th best track of 20016. -->',
