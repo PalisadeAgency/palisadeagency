72c3215391c288824c13f5181e319ffbb704f481
PalisadeAgency <r@palisadeagency.com>
2017-05-22 17:55:13 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -4,7 +4,5 @@ export default [
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1495489800/tristan_ebcg5y.png',
   'https://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
   'https://res.cloudinary.com/drh25z8fo/image/upload/v1492408146/Shall_not_faderrr_cglukg.png',
-  'https://res.cloudinary.com/drh25z8fo/image/upload/v1492373107/dj_boring_front_frriz3.png',
-  'https://res.cloudinary.com/drh25z8fo/image/upload/v1492373308/DJ_Boring_frontttsad_wqusim.png',
  
 ]
