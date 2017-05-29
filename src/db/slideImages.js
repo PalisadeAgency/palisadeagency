444159dab70f80962af052539260b108f052e897
PalisadeAgency <r@palisadeagency.com>
2017-05-29 14:53:06 -0400

    Updated slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,6 +1,6 @@
 export default
 [
-  'https://res.cloudinary.com/drh25z8fo/image/upload/v1493060120/Magoss_dyvis9.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1495684311/borrignggngsg_uf8jln.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1495489792/Harrison_Web_q7muci.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1495489800/tristan_ebcg5y.png',
   'https://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
