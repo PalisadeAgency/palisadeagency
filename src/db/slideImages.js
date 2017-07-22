1d4f28ec639e136794a71a2d8c63a94d2682832a
PalisadeAgency <r@palisadeagency.com>
2017-07-22 01:56:13 -0400

    Update slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,6 +1,6 @@
 export default
 [
-  'http://res.cloudinary.com/drh25z8fo/image/upload/v1500702086/J_Albert_utakrj.png'
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1500702086/J_Albert_utakrj.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1498769482/DJ_BORING_omwhfh.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1495684472/boring_asia_gtky0r.png',
   'https://res.cloudinary.com/drh25z8fo/image/upload/v1492373243/seinfeld_tour_agcyuz.png',
