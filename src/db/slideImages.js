8fec25ed5234275a0ca6bd56eccd08a5556adcb0
PalisadeAgency <r@palisadeagency.com>
2017-09-14 16:31:59 -0400

    Update slideImages.js

--- src/db/slideImages.js
+++ src/db/slideImages.js
@@ -1,6 +1,7 @@
 export default
 [
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1505420960/X-Coast_Page_qslt6a.png',
+  'http://res.cloudinary.com/drh25z8fo/image/upload/v1505421101/Raar_Front_Page_aj1r4x.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1500702086/J_Albert_utakrj.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1498769482/DJ_BORING_omwhfh.png',
   'http://res.cloudinary.com/drh25z8fo/image/upload/v1495684472/boring_asia_gtky0r.png',
